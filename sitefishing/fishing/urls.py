bf0aab8facbda1dd8d372a8b96a41d1f53180f19
leshiivoy <leshiivoy@gmail.com>
2024-07-26 14:59:10 +0200

    Создание проекта

---
+++ sitefishing/fishing/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path, register_converter
+from . import views
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, 'year4')
+
+urlpatterns = [
+    path('', views.index),
+    path('categ/<int:cat_id>/', views.categories),
+    path('categ/<slug:cat_slug>/', views.categories_by_slug),
+    path('archive/<year4:year>/', views.archive),
+]
---
+++ sitefishing/fishing/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('Страница приложения fishing.')
+
+
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
